4f5f9d3f761e62de2e2195c8cf02166eb0401398
Mitch Hankins <mitchhankins92@gmail.com>
2018-10-14 14:36:24 -0600

    Adding date conversion

--- lib/routes/index.ts
+++ lib/routes/index.ts
@@ -1,4 +1,11 @@
 import { Request, Response } from 'express';
+import * as moment from 'moment';
+
+interface Transaction {
+  date: string;
+  marchant: string;
+  amount: string;
+}
 
 export default class Index {
   public routes(app) {
@@ -7,9 +14,22 @@ export default class Index {
     });
 
     app.post('/tx', (req: Request, res: Response) => {
-      const target = req.body.parse.output;
-      console.log(target);
-      res.send(target);
+      const transaction: Transaction = req.body.parse.output;
+
+      const date = transaction.date
+        .split(' ')
+        .map(str => str.replace(/,/g, ''))
+        .map(str => {
+          if (str.match(/^[A-Za-z]+$/)) {
+            return moment()
+              .month(str)
+              .format('M');
+          } else {
+            return str;
+          }
+        });
+      console.log(date);
+      res.send(date);
     });
   }
 }
