570644c31bbd8f9dd73f2e6651c78ee4acd97b09
Mitch Hankins <mitchhankins92@gmail.com>
2018-10-14 19:40:18 -0600

    Enabling cors

--- lib/app.ts
+++ lib/app.ts
@@ -23,6 +23,14 @@ class App {
   private config() {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+      );
+      next();
+    });
   }
 
   private mongoInit(): void {
